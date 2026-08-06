8280387ff8d06ad10ae41cf5f74bcdb8ee9e101b
piaschiavone <piasc@MacBook-Pro-2.local>
2026-08-06 12:07:23 -0300

    Implementación de la operación resta

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,10 +1,7 @@
-﻿public static class Program
+﻿public class Subtraction
 {
-    public static void Main()
-    {
-        Console.WriteLine(Addition.Add(1, 2));
-        Console.WriteLine(Subtraction.Subtract(3, 4));
-        Console.WriteLine(Multiplication.Multiply(5, 6));
-        Console.WriteLine(Division.Divide(7, 8));
-    }
+   public static int Subtract(int a, int b)
+   {
+       return a - b;
+   }
 }
